1900dca78b8d29a473307deabb9a9d686cea7a78
everysick <s.wakeup31@gmail.com>
2016-07-01 22:45:02 +0900

    Impl and rename from leak_memory.c to lmem.c

--- src/lmem.c
+++ src/lmem.c
@@ -1,35 +1,6 @@
-#include "mruby.h"
-#include "mruby/class.h"
-#include "mruby/value.h"
-#include "mruby/data.h"
+#include "lmem.h"
 
-struct collapse
-{
-	int num;
-};
-
-struct balance
-{
-	int num;
-};
-
-static mrb_value mrb_collapse_init(mrb_state *, mrb_value);
-static void mrb_collapse_free(mrb_state *, void *);
-static mrb_value mrb_collapse_get(mrb_state *, mrb_value);
-static mrb_value mrb_collapse_init_copy(mrb_state *, mrb_value);
-
-static mrb_value mrb_balance_init(mrb_state *, mrb_value);
-static void mrb_balance_free(mrb_state *, void *);
-static mrb_value mrb_balance_get(mrb_state *, mrb_value);
-static mrb_value mrb_balance_init_copy(mrb_state *, mrb_value);
-
-// You should change function from mrb_free to custum dfree
-const static struct mrb_data_type mrb_collapse_type = { "Collapse", mrb_collapse_free };
-const static struct mrb_data_type mrb_balance_type = { "Balance", mrb_balance_free };
-
-/*
-  Collapse class's methods.
-*/
+// collapse
 static mrb_value
 mrb_collapse_init(mrb_state *mrb, mrb_value self)
 {
@@ -37,6 +8,11 @@ mrb_collapse_init(mrb_state *mrb, mrb_value self)
 
 	col = (struct collapse *)mrb_malloc(mrb, sizeof(struct collapse));
 	col->num = 1000;
+
+	struct RClass *bal = mrb_class_get(mrb, "Balance");
+	mrb_value cv = mrb_obj_value(bal);
+	mrb_value arg_value = mrb_fixnum_value(100);
+	col->bal = mrb_funcall(mrb, cv, "new", 1, arg_value);
 
 	DATA_TYPE(self) = &mrb_collapse_type;
 	DATA_PTR(self) = col;
@@ -48,6 +24,7 @@ static void
 mrb_collapse_free(mrb_state *mrb, void *ptr)
 {
 	struct collapse *col = ptr;
+	fprintf(stdout, "FREE COLLAPSE\n");
 	mrb_free(mrb, col);
 }
 
@@ -58,6 +35,16 @@ mrb_collapse_get(mrb_state *mrb, mrb_value self)
 	col = DATA_PTR(self);
 
 	return mrb_fixnum_value(col->num);
+}
+
+
+static mrb_value
+mrb_collapse_balance(mrb_state *mrb, mrb_value self)
+{
+	struct collapse *col;
+	col = DATA_PTR(self);
+
+	return col->bal;
 }
 
 static mrb_value
@@ -82,17 +69,17 @@ mrb_collapse_init_copy(mrb_state *mrb, mrb_value copy)
 }
 
 
-/*
-  Balance class's methods.
-*/
+// balance
 static mrb_value
 mrb_balance_init(mrb_state *mrb, mrb_value self)
 {
-
 	struct balance *bal;
+	mrb_int num;
 
 	bal = (struct balance *)mrb_malloc(mrb, sizeof(struct balance));
-	bal->num = 2000;
+
+	mrb_get_args(mrb, "i", &num);
+	bal->num = num;
 
 	DATA_TYPE(self) = &mrb_balance_type;
 	DATA_PTR(self) = bal;
@@ -104,6 +91,7 @@ static void
 mrb_balance_free(mrb_state *mrb, void *ptr)
 {
 	struct balance *bal = ptr;
+	fprintf(stdout, "FREE BALANCE: %d\n", bal->num);
 	mrb_free(mrb, bal);
 }
 
@@ -137,25 +125,23 @@ mrb_balance_init_copy(mrb_state *mrb, mrb_value copy)
 	return copy;
 }
 
-/*
-  Initializer, Finalizer
-*/
+
+
 void
 mrb_lmem_gem_init(mrb_state *mrb)
 {
 	struct RClass *col, *bal;
 
-	// Define struct collapse in mruby
 	col = mrb_define_class(mrb, "Collapse", mrb->object_class);
 	MRB_SET_INSTANCE_TT(col, MRB_TT_DATA);
 	mrb_define_method(mrb, col, "initialize", mrb_collapse_init, MRB_ARGS_NONE());
 	mrb_define_method(mrb, col, "initialize_copy", mrb_collapse_init_copy, MRB_ARGS_NONE());
 	mrb_define_method(mrb, col, "get_value", mrb_collapse_get, MRB_ARGS_NONE());
+	mrb_define_method(mrb, col, "balance", mrb_collapse_balance, MRB_ARGS_NONE());
 
-	// Define struct balance in mruby
 	bal = mrb_define_class(mrb, "Balance", mrb->object_class);
 	MRB_SET_INSTANCE_TT(bal, MRB_TT_DATA);
-	mrb_define_method(mrb, bal, "initialize", mrb_balance_init, MRB_ARGS_NONE());
+	mrb_define_method(mrb, bal, "initialize", mrb_balance_init, MRB_ARGS_REQ(1));
 	mrb_define_method(mrb, bal, "initialize_copy", mrb_balance_init_copy, MRB_ARGS_NONE());
 	mrb_define_method(mrb, bal, "get_value", mrb_balance_get, MRB_ARGS_NONE());
 
